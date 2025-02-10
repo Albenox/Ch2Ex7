8c001d66f47d380a27f8a8b804d0982f9320de08
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2025-02-10 00:15:22 -0500

    Add project files.

---
+++ Ch2Ex7.cpp
@@ -0,0 +1,21 @@
+/*
+    Program File Name: Ch2Ex5.cpp
+    Programmer: Gabriel Inocentes
+    Date: February 9, 2024
+    Requirements:
+    Display the estimated rise of the ocean level in several future years
+*/
+
+#include <iostream>
+using namespace std;
+
+const float rise = 1.5;
+
+int main()
+{
+    double fiveYears = rise * 5;
+    double sevenYears = rise * 7;
+    double tenYears = rise * 10;
+
+    cout << "The estimated rise of the ocean compared to today, will be " << fiveYears << "mm higher in 5 years, " << sevenYears << "mm higher in 7 years, and " << tenYears << "mm higher in 10 years.";
+}
